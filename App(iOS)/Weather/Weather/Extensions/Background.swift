aac6331334fdb93d35c89532372f3d8141e85f3c
Andrei Ulanov <ulanov@live.ru>
2023-07-07 17:41:08 +0300

    feat(App): Add MainCard view

--- App(iOS)/Weather/Weather/Extensions/Background.swift
+++ App(iOS)/Weather/Weather/Extensions/Background.swift
@@ -5,4 +5,71 @@
 //  Created by Andrei Ulanov on 07.07.2023.
 //
 
-import Foundation
+import SwiftUI
+
+enum TimeOfDay {
+    case morning
+    case afternoon
+    case evening
+    case night
+}
+
+struct WeatherBackground: View {
+    @State private var timeOfDay: TimeOfDay = .morning
+    var body: some View {
+        // Определение фона в зависимости от времени суток
+        LinearGradient(gradient: Gradient(colors: getBackgroundColors(for: timeOfDay)), startPoint: .topLeading, endPoint: .bottomTrailing)
+            .ignoresSafeArea()
+            .onAppear {
+                // Проверка времени суток при запуске приложения
+                updateTimeOfDay()
+                // Обновление времени суток при изменении времени
+                Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { _ in
+                    withAnimation {
+                        updateTimeOfDay()
+                    }
+                }
+            }
+            .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { _ in
+                // Проверка времени суток при возвращении в приложение из фона
+                updateTimeOfDay()
+            }
+        
+            
+    }
+    
+    // Функция для получения цветов фона в зависимости от времени суток
+    private func updateTimeOfDay() {
+        let currentHour = Calendar.current.component(.hour, from: Date())
+        
+        switch currentHour {
+        case 6..<12:
+            timeOfDay = .morning
+        case 12..<18:
+            timeOfDay = .afternoon
+        case 18..<22:
+            timeOfDay = .evening
+        default:
+            timeOfDay = .night
+        }
+    }
+    // Функция для определения времени суток
+    private func getBackgroundColors(for timeOfDay: TimeOfDay) -> [Color] {
+        switch timeOfDay {
+        case .morning:
+            return [Color.blue.opacity(0.5), Color(red: 0, green: 0.2, blue: 0.5).opacity(1)]
+        case .afternoon:
+            return [Color(red: 0, green: 0.2, blue: 0.5).opacity(0.5), Color.orange.opacity(1)]
+        case .evening:
+            return [Color.orange.opacity(0.5), Color.purple.opacity(1)]
+        case .night:
+            return [Color.purple.opacity(0.5), Color.blue.opacity(1)]
+        }
+    }
+}
+
+struct WeatherBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherBackground()
+    }
+}
---
+++ App(iOS)/Weather/Weather/Extensions/MainCard.swift
@@ -0,0 +1,70 @@
+//
+//  MainCard.swift
+//  Weather
+//
+//  Created by Andrei Ulanov on 07.07.2023.
+//
+
+import SwiftUI
+
+struct WeatherMainCard: View {
+    @Environment(\.colorScheme) var colorScheme
+    var body: some View {
+        ZStack {
+            
+            VStack(alignment: .leading, spacing: 30) {
+                // City, Country, Date
+                HStack {
+                    Text("London, England")
+                    Spacer()
+                    Text("22.10.2023")
+                }
+               // Temperature
+                HStack {
+                    
+                    Text("23 ºC")
+                        .font(.largeTitle)
+                   // Temperature feel, min, max
+                    VStack(alignment: .leading) {
+                        Text("feel: 23 ºC")
+                        Text("min: 20 ºC")
+                        Text("max: 25 ºC")
+                    }
+                }
+                Spacer()
+                // Condition
+                HStack {
+                    // Huminidity, Clouds
+                    VStack(alignment: .leading) {
+                        Text("Humidity: 100%")
+                        Text("Clouds: 100%")
+                    }
+                    Spacer()
+                    // Visibility, Wind
+                    VStack(alignment: .leading) {
+                        Text("Visibility: 20%")
+                        HStack {
+                            Text("Wind: 4 m/s")
+                            Text("NW")
+                        }
+                    }
+                }
+            }
+            .padding(.all, UIScreen.main.bounds.height/35)
+        }
+        .foregroundColor(Color("FontColor"))
+        .frame(width: UIScreen.main.bounds.width-40, height: UIScreen.main.bounds.height/2, alignment: .center)
+        .blurBackground(style: colorScheme == .dark ? .dark : .light)
+        .cornerRadius(20)
+        .shadow(color: .black, radius: 5)
+    }
+}
+
+struct WeatherMainCard_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.purple.ignoresSafeArea()
+            WeatherMainCard()
+        }
+    }
+}
