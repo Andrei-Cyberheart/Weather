b8372d1401f24c50c7399fbbf7582bb8e8120c76
Andrei Ulanov <ulanov@live.ru>
2023-07-10 11:33:05 +0300

    fix(App): Fix CurrentWeather model

--- App(iOS)/Weather/Weather/Models/CurrentWeather/CurrentWeather.swift
+++ App(iOS)/Weather/Weather/Models/CurrentWeather/CurrentWeather.swift
@@ -21,4 +21,21 @@ struct CurrentWeather: Codable {
     let timezone, id: Int?
     let name: String?
     let cod: Int?
+    
+    private enum CodingKeys: String, CodingKey {
+        case coordinate = "coord"
+        case weather = "weather"
+        case base = "base"
+        case main = "main"
+        case visibility = "visibility"
+        case wind = "wind"
+        case rain = "rain"
+        case clouds = "clouds"
+        case date = "dt"
+        case sys = "sys"
+        case timezone = "timezone"
+        case id = "id"
+        case name = "name"
+        case cod = "cod"
+        }
 }
