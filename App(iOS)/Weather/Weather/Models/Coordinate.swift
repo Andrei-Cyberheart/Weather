9e6c51d717056729e0ca60006731cc8eacb51bbb
Andrei Ulanov <ulanov@live.ru>
2023-07-10 11:49:25 +0300

    feat(App): Add Coordinate model

---
+++ App(iOS)/Weather/Weather/Models/Coordinate.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinate.swift
+//  Weather
+//
+//  Created by Andrei Ulanov on 10.07.2023.
+//
+
+import Foundation
+
+struct Coord: Codable {
+    let lon, lat: Double?
+}
--- App(iOS)/Weather/Weather/Models/CurrentWeather/CurrentWeather.swift
+++ App(iOS)/Weather/Weather/Models/CurrentWeather/CurrentWeather.swift
@@ -8,33 +8,36 @@
 import Foundation
 
 struct CurrentWeather: Codable {
-    let coord: Coord?
-    let weather: [Weather]?
-    let base: String?
-    let main: Main?
-    let visibility: Int?
-    let wind: Wind?
-    let rain: Rain?
-    let clouds: Clouds?
-    let dt: Int?
-    let sys: Sys?
-    let timezone, id: Int?
-    let name: String?
+
+//    let sys: Sys?
+//    let coord: Coord?
+      let name: String?
+    
+//    let weather: [Weather]?
+//    let base: String?
+//    let main: Main?
+//    let visibility: Int?
+//    let wind: Wind?
+//    let rain: Rain?
+//    let clouds: Clouds?
+//    let dt: Int?
+
+    let timezone: Int?
+    
     let cod: Int?
     
     private enum CodingKeys: String, CodingKey {
-        case coordinate = "coord"
-        case weather = "weather"
-        case base = "base"
-        case main = "main"
-        case visibility = "visibility"
-        case wind = "wind"
-        case rain = "rain"
-        case clouds = "clouds"
-        case date = "dt"
-        case sys = "sys"
+//        case coordinate = "coord"
+//        case weather = "weather"
+//        case base = "base"
+//        case main = "main"
+//        case visibility = "visibility"
+//        case wind = "wind"
+//        case rain = "rain"
+//        case clouds = "clouds"
+//        case date = "dt"
+//        case sys = "sys"
         case timezone = "timezone"
-        case id = "id"
         case name = "name"
         case cod = "cod"
         }
