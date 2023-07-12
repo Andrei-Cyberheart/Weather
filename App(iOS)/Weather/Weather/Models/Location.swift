2e7ed8f6784a823b7fee05767982531af366329e
Andrei Ulanov <ulanov@live.ru>
2023-07-12 09:16:07 +0300

    refactor(App): Restructure project files

---
+++ App(iOS)/Weather/Weather/Models/Location.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  Weather
+//
+//  Created by Andrei Ulanov on 10.07.2023.
+//
+
+import Foundation
+
+//struct Location: Codable {
+//    let coord: Coord?
+//    //let sys: Sys?
+//}
