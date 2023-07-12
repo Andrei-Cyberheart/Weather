80bd686ff2e57ba2cdaab53bb61b95f6dcc70731
Andrei Ulanov <ulanov@live.ru>
2023-07-12 09:25:01 +0300

    feat(App): Add Rain model

---
+++ App(iOS)/Weather/Weather/Models/Rain.swift
@@ -0,0 +1,18 @@
+//
+//  Rain.swift
+//  Weather
+//
+//  Created by Andrei Ulanov on 12.07.2023.
+//
+
+import Foundation
+
+struct Rain: Codable {
+    let oneHour: Double?
+    let threeHours: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case oneHour = "1h"
+        case threeHours = "3h"
+    }
+}
