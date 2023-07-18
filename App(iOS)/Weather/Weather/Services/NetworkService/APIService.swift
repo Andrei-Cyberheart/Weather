eaf3fbed69f49eddd059fa970b60aed99bfc8b9a
Andrei Ulanov <ulanov@live.ru>
2023-07-18 08:52:45 +0300

    refactor(App): Refactor getJSON func in APIService
    Refacttor getJSON func in APIService from closure to async/await operators

--- App(iOS)/Weather/Weather/Services/NetworkService/APIService.swift
+++ App(iOS)/Weather/Weather/Services/NetworkService/APIService.swift
@@ -10,40 +10,26 @@ import Foundation
 struct APIService {
     let urlString: String
     
-    func getJSON<T: Decodable>(dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate,
-                                keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys,
-                                completion: @escaping (Result<T, APIError>) -> Void) {
-        guard let url = URL(string: urlString)
-        else {
-            completion(.failure(.invalidURL))
-            return
-        }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard (response as? HTTPURLResponse)?.statusCode == 200
+    func getJSON<T: Decodable>(dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .deferredToDate, keyDecodingStrategy: JSONDecoder.KeyDecodingStrategy = .useDefaultKeys) async throws -> T {
+            guard let url = URL(string: urlString)
             else {
-                completion(.failure(.invalidResponseStatus))
-                return
+                throw APIError.invalidURL
             }
-            guard error == nil
-            else {
-                completion(.failure(.dataTaskError(error!.localizedDescription)))
-                return
-            }
-            guard let data = data
-            else {
-                completion(.failure(.corruptData))
-                return
-            }
-            let decoder = JSONDecoder()
-            decoder.dateDecodingStrategy = dateDecodingStrategy
-            decoder.keyDecodingStrategy = keyDecodingStrategy
             do {
-                let decodedData = try decoder.decode(T.self, from: data)
-                completion(.success(decodedData))
+                let (data, response) = try await URLSession.shared.data(from: url)
+                guard (response as? HTTPURLResponse)?.statusCode == 200
+                else { throw APIError.invalidResponseStatus }
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = dateDecodingStrategy
+                decoder.keyDecodingStrategy = keyDecodingStrategy
+                do {
+                    let decodedData = try decoder.decode(T.self, from: data)
+                    return decodedData
+                } catch {
+                    throw APIError.decodingError(error.localizedDescription)
+                }
             } catch {
-                completion(.failure(.decodingError(error.localizedDescription)))
+                throw APIError.dataTaskError(error.localizedDescription)
             }
         }
-        .resume()
-    }
 }
