79f69476e6f3c2024fe824864f15a20476b4e615
Andrei Ulanov <ulanov@live.ru>
2023-08-01 10:49:29 +0300

    feature: initial worker project Weather.Service.ExthernalAPIClient

---
+++ Service/WeatherService/WeatherService.ExternalAPIClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using WeatherService.ExternalAPIClient;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
+
