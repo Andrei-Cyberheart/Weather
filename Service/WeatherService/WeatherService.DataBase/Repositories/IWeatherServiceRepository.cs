08f6c67cecff52a7265c9225f23176357dbfdbcb
Andrei Ulanov <ulanov@live.ru>
2023-08-03 13:09:15 +0300

    feature(Service.DataBase): add IWeatherServiceRepository

---
+++ Service/WeatherService/WeatherService.DataBase/Repositories/IWeatherServiceRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WeatherService.DataBase.Repositories
+{
+	public interface IWeatherServiceRepository
+	{
+        Task<WeatherService> GetWeatherServiceByCoordinates(double latitude, double longitude);
+        Task SaveWeatherForecast(WeatherService forecast);
+    }
+}
+
