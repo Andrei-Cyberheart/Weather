7e74f2334d3faa1f45b830e612718659672d5e97
Andrei Ulanov <ulanov@live.ru>
2023-08-03 13:17:19 +0300

    feature(Service.ExthernalAPIClient): add OpenWeatherMapApiClient
    Client for openweathermap.com service

---
+++ Service/WeatherService/WeatherService.ExternalAPIClient/Clients/OpenWeatherMapApiClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace WeatherService.ExternalAPIClient.Clients
+{
+    public class OpenWeatherMapApiClient : IWeatherApiClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey = "YOUR_OPENWEATHERMAP_API_KEY";
+
+        public OpenWeatherMapApiClient()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://api.openweathermap.org/data/2.5/");
+        }
+
+        public async Task<WeatherService> GetWeatherForecastByCoordinatesAsync(double latitude, double longitude)
+        {
+            var response = await _httpClient.GetAsync($"weather?lat={latitude}&lon={longitude}&appid={_apiKey}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return null; // or handle the error appropriately
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var apiResponse = JsonConvert.DeserializeObject<WeatherApiResponse>(content);
+
+            return apiResponse.ToWeatherForecast();
+        }
+    }
+}
+
