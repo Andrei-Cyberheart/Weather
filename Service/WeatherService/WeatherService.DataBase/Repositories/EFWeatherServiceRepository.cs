690306ef41273395f997ecb03520aa3a96147ccb
Andrei Ulanov <ulanov@live.ru>
2023-08-03 13:10:18 +0300

    feature(Service.DataBase): add EFWeatherServiceRepository

---
+++ Service/WeatherService/WeatherService.DataBase/Repositories/EFWeatherServiceRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace WeatherService.DataBase.Repositories
+{
+    public class EFWeatherForecastRepository : IWeatherServiceRepository
+    {
+        private readonly DbContext _dbContext;
+
+        public EFWeatherForecastRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<WeatherService> GetWeatherForecastByCoordinates(double latitude, double longitude)
+        {
+            return await _dbContext.WeatherForecasts
+                .FirstOrDefaultAsync(wf => wf.Latitude == latitude && wf.Longitude == longitude);
+        }
+
+        public async Task SaveWeatherForecast(WeatherService forecast)
+        {
+            _dbContext.WeatherForecasts.Add(forecast);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
+
