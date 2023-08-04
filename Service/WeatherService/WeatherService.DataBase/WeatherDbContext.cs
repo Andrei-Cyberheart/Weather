04e5bb8ed7c38e8452857996c6f01ddf6b8e97a2
Andrei Ulanov <ulanov@live.ru>
2023-08-04 14:59:22 +0300

    feature(Service.DataBase): add WeatherDbContext

---
+++ Service/WeatherService/WeatherService.DataBase/WeatherDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WeatherService.DataBase
+{
+    public class WeatherDbContext : DbContext
+    {
+        public WeatherDbContext(DbContextOptions<WeatherDbContext> options) : base(options) { }
+
+        public DbSet<WeatherService> WeatherForecasts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure entity properties, relationships, etc. if needed
+            // For example, you might want to set a primary key for the WeatherForecast entity.
+        }
+    }
+}
+
