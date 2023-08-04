970e4a7daff466e656c817bf5cf18f94785a42de
Andrei Ulanov <ulanov@live.ru>
2023-08-04 14:58:07 +0300

    feature(Service.API): add WeatherService model

---
+++ Service/WeatherService/WeatherService.API/Models/WeatherService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace WeatherService.API.Models
+{
+    public class WeatherService
+    {
+        public int Id { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public double Temperature { get; set; }
+        public string Description { get; set; }
+    }
+}
+
