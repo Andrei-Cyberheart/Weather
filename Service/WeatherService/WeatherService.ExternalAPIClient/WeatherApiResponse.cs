6e3636e9c91e78d156a4e7d84025db20c697dc98
Andrei Ulanov <ulanov@live.ru>
2023-08-04 14:58:46 +0300

    feature(Service.ExthernalAPIClient): add WeatherApiResponse model

---
+++ Service/WeatherService/WeatherService.ExternalAPIClient/WeatherApiResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace WeatherService.ExternalAPIClient
+{
+    public class WeatherApiResponse
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+        public string Name { get; set; }
+        public long Dt { get; set; }
+        public Main Main { get; set; }
+        public List<Weather> Weather { get; set; }
+
+        public WeatherService ToWeatherForecast()
+        {
+            return new WeatherService
+            {
+                Latitude = Lat,
+                Longitude = Lon,
+                Location = Name,
+                Date = DateTimeOffset.FromUnixTimeSeconds(Dt).DateTime,
+                Temperature = Main.Temp,
+                Description = Weather?.FirstOrDefault()?.Description
+            };
+        }
+    }
+}
+
